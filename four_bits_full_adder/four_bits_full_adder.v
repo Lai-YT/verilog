daea43323325039a38da42cc158e49261d37a545
Lai-YT <381xvmvbib@gmail.com>
2021-04-09 09:01:09 +0800

    dataflow -> behavioral

--- four_bits_full_adder/four_bits_full_adder.v
+++ four_bits_full_adder/four_bits_full_adder.v
@@ -16,7 +16,12 @@ module full_adder(sum, c_out, a, b, c_in);
 
 	input a, b, c_in;
 	output sum, c_out;
+	reg sum, c_out;
 	
-	assign {c_out, sum} = a + b + c_in;
+	//	behavioral modeling
+	always @(a or b)
+	begin
+		{c_out, sum} = a + b + c_in;
+	end
 	
 endmodule
